998de357b99000ba231b23c171fe2969a8468254
franciscob1218 <franciscob1218@gmail.com>
2022-11-28 20:47:29 -0600

    Vue.js + Vite project was created

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import Navbar from "./components/Nav_Bar.vue";
+</script>
+
+<template>
+  <header>
+
+    <Navbar />
+
+    <RouterView />
+
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+//Vue object that creates a application it is passed the application from App.vue
+const app = createApp(App);
+
+//This uses the .use() method to include/run the code in router. 
+app.use(router);
+
+//mounts the app to the appropriate div
+app.mount("#app");
