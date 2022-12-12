2d5fb7a97577ad0122206766f900fb96b2a24968
Franciscob1218 <Francsicob1218@gmail.com>
2022-12-11 19:48:42 -0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import Navbar from "./components/Nav_bar.vue";
-import NeivLogo from "./components/icons/NeivLogoPlain.svg";
 </script>
 
 <template>
-  <header :class="[$style.header]">
-    <img :class="[$style.logo]" src="./components/icons/NeivLogoPlain.svg" />`
 
-    <Navbar />
+  <header>
+    
   </header>
 
-  <body :class="[$style.body]">
-    <RouterView />
+  <nav>
+    
+  </nav>
+
+  <body>
+
   </body>
 
   <footer>
-    <a></a>
+
   </footer>
+
 </template>
 
-<style module>
-.logo {
-  height: 120px;
-}
-.header {
-  border: 1px solid red;
-
-  width: 100vw;
-  height: 12vh;
-
-  display: flexbox;
-  flex-direction: row;
-  flex-wrap: nowrap;
-}
-.body {
-  border: 1px solid blue;
-
-  margin: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: #e6e2d6;
-  display: flexbox;
-}
-</style>
+<style module></style>
