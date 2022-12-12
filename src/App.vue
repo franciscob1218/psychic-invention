86e09fccecedacaf842e3826387f7745332a43c9
Franciscob1218 <Francsicob1218@gmail.com>
2022-12-11 20:37:49 -0600

    sdad

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,9 @@ import NeivLogo from "./components/icons/NeivLogoPlain.svg";
 </script>
 
 <template>
-  <header :class="[$style.header]">
-    <img :class="[$style.logo]" src="./components/icons/NeivLogoPlain.svg" />`
 
-    <Navbar />
-  </header>
-
-  <body :class="[$style.body]">
-    <RouterView />
-
-  <footer>
-    <a></a>
-  </footer>
 </template>
 
 <style module>
-.logo {
-  height: 120px;
-}
-.header {
-  border: 1px solid red;
 
-  width: 100vw;
-  height: 12vh;
-
-  display: flexbox;
-  flex-direction: row;
-  flex-wrap: nowrap;
-}
-.body {
-  border: 1px solid blue;
-
-  margin: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: #e6e2d6;
-  display: flexbox;
-}
 </style>
